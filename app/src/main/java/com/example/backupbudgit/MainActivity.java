386b6a8cfdf7a1554b8b942ea711f7579788aafa
Nick <115027712+Foley-ops@users.noreply.github.com>
2024-04-23 12:11:03 -0500

    add progress

--- app/src/main/java/com/example/backupbudgit/MainActivity.java
+++ app/src/main/java/com/example/backupbudgit/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.backupbudgit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        usernameEditText = findViewById(R.id.loginpageUsername);
+        passwordEditText = findViewById(R.id.loginpagePassword);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
         });
+    }
+
+    private void loginUser() {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // Here you can add your logic for validating login credentials
+        if(username.equals("admin") && password.equals("admin123")) {
+            // Login success
+            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+            // Intent to navigate to another activity
+        } else {
+            // Login failed
+            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+        }
     }
 }
